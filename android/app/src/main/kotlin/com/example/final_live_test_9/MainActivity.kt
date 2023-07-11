b1f214298ae73ddff1998fb1dee46fc64025f99c
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-11 10:34:01 +0600

    env setup done

---
+++ android/app/src/main/kotlin/com/example/final_live_test_9/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_live_test_9
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'live test 9',
+      home: HomeScreen(),
+    );
+  }
+
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Live Test 9"),
+      ),
+    );
+  }
+}
+
